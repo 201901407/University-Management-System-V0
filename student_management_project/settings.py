577c3c8e4d4ef36de52266bae8de05d1776cb593
Darshil Parikh <201901407@daiict.ac.in>
2022-06-06 09:09:02 +0530

    Update settings.py

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#k429(qix$ezdkv*x7vvff^dufvzudapfabwc1fvff!(u@-l4a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
